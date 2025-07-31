d8a6669ce32fdeedb29c1b4f160ed0e16ffa2305
link <johannsilvamendez@gmail.com>
2025-07-31 17:34:21 -0500

    functions jwt

--- api_inventario/middlewares/JWT.js
+++ api_inventario/middlewares/JWT.js
@@ -7,9 +7,37 @@ const generarJWT = function (uid) {
             if (err) {
                 console.log(err)
                 reject("no se genero el token")
-            }else{
+            } else {
                 resolve(token)
             }
         }
     })
+}
+
+const validar = async (req, res, next) => {
+    const token = req.header("x-token");
+    if (!token) {
+        return res.status(401).json({
+            msg: "No hay token en la peticion"
+        })
+    }
+    try {
+        const {uid}= jwt.verify(token, proccess.env.SECRETORPRIVATEKEY)
+        let usuario = await Holder.findByid(uid);
+        if(!usuario){
+            return res.status(401).json({
+                msg:"usuario no existe"
+            })
+        }
+        if(usuario.estado == 0){
+          return res.status(401).json({
+            msg:"usuario inactivo"
+          })
+        }
+        next();
+    } catch (error) {
+        res.status(401).json({
+            msg:"token no valido"
+        })
+    }
 }
